037984002d5666c1b77bdf63bafa456490a5a3b0
shaogbi <bishaogang@gmail.com>
2015-02-07 00:18:36 +0800

    Update query-mysql.js

--- Node.js/query-mysql.js
+++ Node.js/query-mysql.js
@@ -6,13 +6,15 @@ var connection = mysql.createConnection({
   password: "<password>"
 });
 connection.connect();
-var sql = "select * from blog.user limit 5";
-connection.query(sql, function(err, res, fields) {
+var sql = "select * from user where name = ? and age > ?",
+  attrs = ["Tom", 10];
+connection.query(sql, attrs, function(err, res, fields) {
   if(err) throw err;
   console.log(res.constructor.name); // Array
   console.log(res.length); // 5
   console.log(res); // [{id: 1, name: 'Tom', age: 25}, {...}, ...]
   console.log(res[0].constructor.name); // RowDataPacket
   console.log("Result data sample: ", res[0]); // {id: 1, name: 'Tom', age: 25}
+  console.log("Age is ", res[0]["age"]); // 25
 });
 connection.end();
